56e3766da46309b6f025e1b7e09686f8d66380f2
Agnieszka Korczak <korczak.a.m@gmail.com>
2019-02-03 00:41:49 +0100

    Add project content

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID':'514342398271-4vgh1ije8tdl59efj3sgaqh9qq9hqldt.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'f8cGe6OKsQb9hYy7J4zbyntW',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
